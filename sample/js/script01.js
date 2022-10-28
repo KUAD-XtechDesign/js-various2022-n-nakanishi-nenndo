29c1aef93efac562cba80b033c40444311c7383e
n-nakanishi-nenndo <n-nakanishi2278@st.kyoto-art.ac.jp>
2022-10-28 17:39:39 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -51,3 +51,36 @@ var bar = new ProgressBar.Line(splash_text, {//id名を指定
   bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
     $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
   });  
+
+  //lightbox オプションの設定※https://lokeshdhakar.com/projects/lightbox2/#options参照
+
+lightbox.option({
+    'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
+    'albumLabel': ' %1 / total %2 '//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
+  })
+  
+  //ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
+  
+  function fadeAnime(){
+  // flipLeft
+  $('.gallery li').each(function(){ 
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+          $(this).addClass('flipLeft');
+      }else{
+          $(this).removeClass('flipLeft');
+      }
+  });
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+  // ページが読み込まれたらすぐに動かしたい場合の記述
+    $(window).on('load', function(){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまでページが読み込まれたらすぐに動かしたい場合の記述
