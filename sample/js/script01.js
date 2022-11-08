18dd89009c8be7fa8f1ce0a2bd7dd755f75901a4
n-nakanishi-nenndo <n-nakanishi2278@st.kyoto-art.ac.jp>
2022-11-08 15:53:20 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,26 +1,3 @@
-
-$('.slider').slick({
-    fade:true,//切り替えをフェードで行う。初期値はfalse。
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-    speed:1000,//スライドの動きのスピード。初期値は300。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 1,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    arrows: true,//左右の矢印あり
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    dots: true,//下部ドットナビゲーションの表示
-        pauseOnFocus: false,//フォーカスで一時停止を無効
-        pauseOnHover: false,//マウスホバーで一時停止を無効
-        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-});
-
-//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-    $('.slider').slick('slickPlay');
-});
-
 //テキストのカウントアップ+バーの設定
 var bar = new ProgressBar.Line(splash_text, {//id名を指定
     easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
@@ -49,38 +26,8 @@ var bar = new ProgressBar.Line(splash_text, {//id名を指定
   
   //アニメーションスタート
   bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-    $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
-  });  
-
-  //lightbox オプションの設定※https://lokeshdhakar.com/projects/lightbox2/#options参照
-
-lightbox.option({
-    'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
-    'albumLabel': ' %1 / total %2 '//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
-  })
-  
-  //ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
-  
-  function fadeAnime(){
-  // flipLeft
-  $('.gallery li').each(function(){ 
-      var elemPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight){
-          $(this).addClass('flipLeft');
-      }else{
-          $(this).removeClass('flipLeft');
-      }
+    $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+    $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+    $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+    $("#splash").fadeOut();//#splashエリアをフェードアウト
   });
-  }
-  
-  // 画面をスクロールをしたら動かしたい場合の記述
-    $(window).scroll(function (){
-      fadeAnime();/* アニメーション用の関数を呼ぶ*/
-    });// ここまで画面をスクロールをしたら動かしたい場合の記述
-  
-  // ページが読み込まれたらすぐに動かしたい場合の記述
-    $(window).on('load', function(){
-      fadeAnime();/* アニメーション用の関数を呼ぶ*/
-    });// ここまでページが読み込まれたらすぐに動かしたい場合の記述
